43eab6e18a5ee21370fd50bd11e976525f7a4b8e
Stephen Ascher <sixsecondfiesta@gmail.com>
2013-12-21 22:29:21 -0500

    Fix grouping undo deprecation warnings.

--- Editor/Grouping.cs
+++ Editor/Grouping.cs
@@ -8,25 +8,22 @@ public class Grouping
 {	
 	[MenuItem ("Utility/Group Selected %g", false, 1)]
 	static void GroupSelected()
-	{
-		Undo.CreateSnapshot();
-		
-		//cache selected objects in scene:
+	{	
+		//Cache selected objects in scene.
 		Transform[] selectedObjects = Selection.transforms;
 		
-		//early exit if nothing is selected:
+		//Early out if nothing is selected.
 		if(selectedObjects.Length == 0)
 		{
 			return;
 		}
-		
-
-		Undo.RegisterSceneUndo("Group selected");
-		
+				
 		Vector3 averagePosition = Vector3.zero;
-		//parent construction and hierarchy structure decision:
 		bool nestParent = true;
 		Transform newGroupTransform = new GameObject("Group").transform;
+
+		Undo.RegisterCreatedObjectUndo(newGroupTransform, "Create group parent");
+
 		Transform coreParent = selectedObjects[0].parent;
 		foreach(Transform item in selectedObjects)
 		{
@@ -39,43 +36,38 @@ public class Grouping
 
 		if(nestParent)
 		{
-			newGroupTransform.parent = coreParent;
+			Undo.SetTransformParent(newGroupTransform, coreParent, "Set group parent's parent");
 			newGroupTransform.position = averagePosition / selectedObjects.Length;
 		}
 		else
 		{
-			//place group's pivot on the active transform in the scene:
+			//Place group's pivot on the active transform in the scene.
 			newGroupTransform.position = Selection.activeTransform.position;
 		}
 		
-		//set selected objects as children of the group:
+		//Set selected objects as children of the group.
 		foreach(Transform item in selectedObjects)
 		{
-			item.parent = newGroupTransform;
+			Undo.SetTransformParent(item, newGroupTransform, "Group Selected");
 		}
 	}
 
 	[MenuItem ("Utility/Ungroup Selected %#g", false, 2)]
 	static void UngroupSelected()
 	{
-		Undo.CreateSnapshot();
-		
 		Transform activeSelection = Selection.activeTransform;
 
-		//early out if there's nothing to do.
+		//Early out if there's nothing to do.
 		if(activeSelection.childCount == 0)
 		{
 			Debug.Log("Ungroup " + activeSelection.name + ": selected object has nothing to ungroup!");
 			return;
 		}
 		
-		Undo.RegisterSceneUndo("Ungroup " + activeSelection.name);
-
 		Transform selectedParent = activeSelection.parent;
 
-		//We store each child in a list, since otherwise we'd mess with the
-		//enumerable state while enumerating it!
-
+		//Store each child in a list, since otherwise we'd mess with the
+		//enumerable state while enumerating it.
 		List<Transform> toUnparent = new List<Transform>();
 
 		foreach(Transform child in activeSelection)
@@ -85,7 +77,7 @@ public class Grouping
 
 		foreach(Transform child in toUnparent)
 		{
-			child.parent = selectedParent;
+			Undo.SetTransformParent(child, selectedParent, "Ungroup " + activeSelection.name);
 		}
 
 		if(activeSelection.GetComponents<Component>().Length > 1)
@@ -94,7 +86,7 @@ public class Grouping
 		}
 		else
 		{
-			Object.DestroyImmediate(activeSelection.gameObject);
+			Undo.DestroyObjectImmediate(activeSelection.gameObject);
 		}
 	}
 
