7f2f5bd9726247e4d6de9b545f1621387343bc31
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-11 17:27:02 -0400

    Added a signed angle between two vector3s function

--- Mathy.cs
+++ Mathy.cs
@@ -128,4 +128,14 @@ public static class Mathy
 		return (a0*t*tsq+a1*tsq+a2*t+a3);
 	}
 	*/
+    /// <summary>
+    /// Determine the signed angle between two vectors, with normal 'n'
+    /// as the rotation axis.
+    /// </summary>
+    public static float AngleSigned(Vector3 v1, Vector3 v2, Vector3 n)
+    {
+        return Mathf.Atan2(
+            Vector3.Dot(n, Vector3.Cross(v1, v2)),
+            Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
+    }
 }
