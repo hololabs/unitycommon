5ac79795285d0363bd712f5a6d7aa55d8e128ed9
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-20 12:00:29 -0400

    Added a deselect all menu item (shortcut is a)

---
+++ Editor/DeselectAll.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+ 
+public class DeselectAll
+{
+    [MenuItem ("Edit/Deselect All _a")]
+    static void DoDeselect()
+    {
+        Selection.objects = new UnityEngine.Object[0];
+    }
+}
