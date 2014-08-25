2a5e1a0b325cd9bc60d9bc62eca514cf5f2f580b
Stephen Ascher <sixsecondfiesta@gmail.com>
2014-08-25 02:20:00 -0400

    CopyTransformPathToClipboard

---
+++ Editor/MenuItemCopyTransformPathToClipboard.cs
@@ -0,0 +1,22 @@
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+public static class MenuItemCopyTransformPathToClipboard
+{
+    [MenuItem("Utility/Copy Transform Path to Clipboard", false, 150)]
+    static void CopyTransformPathToClipboard()
+    {
+        var selection = Selection.activeTransform;
+
+        var sb = new StringBuilder();
+        sb.Append(selection.name);
+        while(selection.parent != null) {
+            selection = selection.parent;
+            sb.Insert(0, selection.name + "/");
+        }
+        EditorGUIUtility.systemCopyBuffer = sb.ToString();
+    }
+}
